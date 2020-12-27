c67ef22fc83f039b1bb997181cfb544e34198878
Clovis <clovis@byte49.exposed>
2020-12-27 18:14:23 +0100

    Starting on good basis for frontend

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 
 import 'tailwindcss/dist/tailwind.css';
 import '@fortawesome/fontawesome-free/css/all.css';
-import List from './list/List.jsx';
 
 window.onload = () => {
-	ReactDOM.render(<List />, document.querySelector('#mount'))
+	// ReactDOM.render(<List />, document.querySelector('#mount'))
 };
